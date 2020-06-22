52315b9b7186fe37d10a72a646a546db459ca30b
alaa akiel <alaa.21.iraq@gmail.com>
2020-06-22 22:19:21 +0300

    modified:   route.go

--- route.go
+++ route.go
@@ -3,9 +3,15 @@ package restful
 import "github.com/gofiber/fiber"
 
 
-func Resource(prefix string, group *fiber.Group, controller InterfaceResource) *fiber.Group {
+func Resource(prefix string, group *fiber.Group, 
+		controller InterfaceResource, handlers ...fiber.Handler) *fiber.Group {
 
 	resource := group.Group(prefix)
+
+	for handler := range handlers {
+
+		resource.Use(handler)
+	}
 
 	resource.Get("/", controller.Get)
 	resource.Post("/", controller.Post)
